16533c3b5e572ea835c0ab68e1dd878e6c7d882f
Sergioguijarro87 <75782727+Sergioguijarro87@users.noreply.github.com>
2021-05-13 10:37:45 +0200

    Update leerFicheroXML.js

--- carmenFRP.github.io-main1/leerFicheroXML.js
+++ carmenFRP.github.io-main1/leerFicheroXML.js
@@ -5,10 +5,4 @@ function gestionarFicheroXML(xmlDoc){
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + alumnos[i].textContent + "</p>"
 	
 }
-
-let capa = document.querySelector("div:nth-child(1)") 
-capa.addEventListener("click",CargarFichero);
-function CargarFichero()
-{
 	loadDocA("libros.xml","xml");
-}
